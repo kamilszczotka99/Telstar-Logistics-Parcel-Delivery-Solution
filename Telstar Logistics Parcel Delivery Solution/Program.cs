52598f832eee98212f66b8a1a4a899161ab80230
Kamil Szczotka <kasz@netcompany.com>
2023-01-12 19:56:57 +0100

    moved connectionstring to seperate file

--- Telstar Logistics Parcel Delivery Solution/Program.cs
+++ Telstar Logistics Parcel Delivery Solution/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Telstar_Logistics_Parcel_Delivery_Solution.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("connectionstrings.json", optional: false, reloadOnChange: true);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(connectionString)
+);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
