2f928def0f30bbc622e81d02b1902ff2794298ed
Kamil Szczotka <kasz@netcompany.com>
2023-01-13 04:07:09 +0100

    added Cors

--- Telstar Logistics Parcel Delivery Solution/Program.cs
+++ Telstar Logistics Parcel Delivery Solution/Program.cs
@@ -16,6 +16,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000") 
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 builder.Services.AddScoped<RouteMapperService, RouteMapperServiceImpl>();
 
 var app = builder.Build();
@@ -26,6 +37,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors();
+
 
 app.UseHttpsRedirection();
 
