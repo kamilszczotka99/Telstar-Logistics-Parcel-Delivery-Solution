6015b886e1419593112f6ab2a6a8a3c34453c800
stvu <stvu@netcompany.com>
2023-01-13 04:38:30 +0100

    Integration v1.1

--- Telstar Logistics Parcel Delivery Solution/Controllers/OrderInformationController.cs
+++ Telstar Logistics Parcel Delivery Solution/Controllers/OrderInformationController.cs
@@ -1,7 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Newtonsoft.Json;
-
+using Telstar_Logistics_Parcel_Delivery_Solution.Data;
+using Telstar_Logistics_Parcel_Delivery_Solution.Models;
 
 namespace Telstar_Logistics_Parcel_Delivery_Solution.Controllers
 {
@@ -9,6 +10,15 @@ namespace Telstar_Logistics_Parcel_Delivery_Solution.Controllers
     public class OrderInformationController : Controller
     {
         private const double maxWeight = 40.0;
+        private ApplicationDbContext applicationDbContext;
+        private List<Category> categories;
+        private List<City> citys;
+
+        public OrderInformationController(ApplicationDbContext applicationDbContext)
+        {
+            categories = applicationDbContext.Categories.ToList();
+            citys = applicationDbContext.CITY.ToList();
+        }
 
         [HttpPost]
         [Route("/integration/india")]
@@ -37,9 +47,10 @@ namespace Telstar_Logistics_Parcel_Delivery_Solution.Controllers
         }
 
         [HttpPost]
-        [Route("/information/order")]
+        [Route("/route/information")]
         public ActionResult Post([FromBody] ExternalOrderRequestDto externalOrder)
         {
+            sanitize(externalOrder);
             if (!validate(externalOrder))
             {
                 return BadRequest(new ErrorResponseDto("This went bad, sorry"));
@@ -47,6 +58,16 @@ namespace Telstar_Logistics_Parcel_Delivery_Solution.Controllers
 
             return Ok(createResponse());
 
+        }
+
+        private void sanitize(ExternalOrderRequestDto externalOrderDto)
+        {
+            externalOrderDto.cityTo=externalOrderDto.cityTo.ToUpper();
+            externalOrderDto.cityFrom=externalOrderDto.cityFrom.ToUpper();
+
+            for(int i = 0; i<externalOrderDto.categories.Count; i++) {
+                externalOrderDto.categories[i]= externalOrderDto.categories[i].ToUpper();
+            }
         }
 
         private bool validate(ExternalOrderRequestDto externalOrderDto)
@@ -61,25 +82,46 @@ namespace Telstar_Logistics_Parcel_Delivery_Solution.Controllers
             if (externalOrderDto.categories.Contains("weapon")) { return false; }
 
 
-            /*List<string> destination_list = new List<string> { "Dest_1", "Dest_2" };
+            
+            List<String> normalised_categories = new List<String>();
+            foreach (var category in categories)
+            {
+                normalised_categories.Add(category.NormalizedCategoryType.ToUpper());
+            }
 
-            if (!IsInList(externalOrderDto.start_destination, destination_list)) { return false; }
-            if (!IsInList(externalOrderDto.stop_destination, destination_list)) { return false; }*/
+            
+            List<String> normalised_cities = new List<String>();
+            foreach (var city in citys)
+            {
+                normalised_cities.Add(city.NormalizedName.ToUpper());
+            }
 
+            if (!CompareLists(externalOrderDto.categories, normalised_categories)) { return false; }
+
+            if (!IsInList(externalOrderDto.cityFrom, normalised_cities)) { return false; }
+            if (!IsInList(externalOrderDto.cityTo, normalised_cities)) { return false; }
+            
             return true;
         }
 
-        public static bool IsNumber(string input)
+        private static bool IsNumber(string input)
         {
             int result1;
             double result2;
             return int.TryParse(input, out result1) || double.TryParse(input, out result2);
         }
 
-        public static bool IsInList(string input, List<string> list)
+        private static bool IsInList(string input, List<string> list)
         {
             return list.Contains(input);
         }
+
+        private static bool CompareLists(List<string> incomingList, List<string> referenceList)
+        {
+            // Use the All method to check if all elements in the incoming list exist in the reference list
+            return incomingList.All(referenceList.Contains);
+        }
+
 
         private ExternalOrderResponseDto createResponse()
         {
